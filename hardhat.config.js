e4c4d3040e895720fa2f72b077a91077ed126fb9
Tayfun Sur <tayfunsur@gmail.com>
2022-12-02 15:16:18 +0300

    Update hardhat.config

---
+++ hardhat.config.js
@@ -0,0 +1,15 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config({ path: ".env" });
+
+const NODE_HTTP_URL = process.env.NODE_HTTP_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+
+module.exports = {
+  solidity: "0.8.9",
+  networks: {
+    goerli: {
+      url: NODE_HTTP_URL,
+      accounts: [PRIVATE_KEY],
+    },
+  },
+};
